eb9a7f3b8af9e07bf5e0219576a53f7cf1c43d41
FaabScheeren <f.scheeren@outlook.com>
2020-04-20 15:21:21 +0200

    Select recipe details from store

--- src/screens/RecipeDetailsScreen.js
+++ src/screens/RecipeDetailsScreen.js
@@ -2,11 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { View, Text, StyleSheet } from "react-native";
 import { getRecipeDetails } from "../store/recipes/actions";
+import { recipeDetailsSelector } from "../store/recipes/selectors";
 
 function RecipeDetailsScreen({ navigation, route }) {
   const dispatch = useDispatch();
+  const selectDetails = useSelector(recipeDetailsSelector);
   const { recipeId } = route.params;
+
   console.log("recipe id", recipeId);
+  console.log("recipe details", selectDetails);
 
   useEffect(() => {
     dispatch(getRecipeDetails(recipeId));
--- src/store/recipes/selectors.js
+++ src/store/recipes/selectors.js
@@ -1,1 +1,2 @@
 export const recipeSelector = (state) => state.recipes.recipes;
+export const recipeDetailsSelector = (state) => state.recipes.recipeDetails;
