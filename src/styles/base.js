4afd59809948ceb8e75103bab422d10a955f7587
FaabScheeren <f.scheeren@outlook.com>
2020-04-23 17:50:04 +0200

    Styling account screen

--- src/styles/base.js
+++ src/styles/base.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Dimensions } from "react-native";
 
 export const colors = {
-  primary: "",
-  secondary: "",
-  tertiary: "",
+  primary: "#95c6b1",
+  // secondary: "",
+  // tertiary: "",
 };
 
 export const spaces = {
@@ -16,10 +16,10 @@ export const spaces = {
 export const fonts = {
   sm: 12,
   md: 18,
-  lg: 28,
+  lg: 24,
   xl: 35,
   header: "poppins",
-  subHeader: montserrat,
+  subHeader: "montserrat",
   subHeader2: "montserrat-semi",
   text: "open-sans",
 };
