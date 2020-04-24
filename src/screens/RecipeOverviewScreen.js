7aeb870e6450818177f4de566e62223fbd4c3545
FaabScheeren <f.scheeren@outlook.com>
2020-04-24 09:44:21 +0200

    Styling recipe overview screen

--- src/screens/RecipeOverviewScreen.js
+++ src/screens/RecipeOverviewScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { Card, Image } from "react-native-elements";
 import { selectUserCategories } from "../store/user/selector";
+import { colors, spaces, fonts } from "../styles/base";
 
 function RecipeOverviewScreen({ navigation, route }) {
   const { categoryId } = route.params;
@@ -21,7 +28,7 @@ function RecipeOverviewScreen({ navigation, route }) {
 
   return (
     <View>
-      <Text>
+      <Text style={styles.header}>
         This is the recipe overview page. Take a look of your own recipe and
         enjoy!
       </Text>
@@ -38,14 +45,14 @@ function RecipeOverviewScreen({ navigation, route }) {
               })
             }
           >
-            <Card>
+            <Card containerStyle={styles.card}>
               <Image
+                style={styles.cardImage}
                 source={{
                   uri: `${item.media[0].file_name}`,
                 }}
-                style={{ height: 145, width: 145 }}
               />
-              <Text style={{ width: 145 }}>{item.title}</Text>
+              <Text style={styles.cardSubTitle}>{item.title}</Text>
             </Card>
           </TouchableOpacity>
         )}
@@ -54,5 +61,27 @@ function RecipeOverviewScreen({ navigation, route }) {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  header: {
+    fontFamily: fonts.header,
+    fontSize: fonts.md,
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  cardImage: { height: 175, width: 175 },
+  card: {
+    padding: 0,
+    borderColor: colors.primary,
+    borderWidth: 0.4,
+  },
+  cardSubTitle: {
+    fontFamily: fonts.subHeader,
+    fontSize: fonts.sm,
+    // paddingHorizontal: 15,
+    // paddingBottom: 15,
+    padding: 15,
+  },
+  contentContainerStyle: { paddingBottom: spaces.lg },
+});
 
 export default RecipeOverviewScreen;
